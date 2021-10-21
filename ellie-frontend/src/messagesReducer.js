1e653c55e726c3d631946f0efdce9766f31a00d8
Your Name <you@example.com>
2021-10-21 14:35:59 +0300

    admin panel fixed

--- ellie-frontend/src/messagesReducer.js
+++ ellie-frontend/src/messagesReducer.js
@@ -1,30 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit';
-
+import { createSlice } from "@reduxjs/toolkit";
 
 export const slice = createSlice({
-    name: 'message',
-    initialState: {
-      value:[],
+  name: "message",
+  initialState: {
+    value: [],
+    adminMsgs: [],
+  },
+  reducers: {
+    messageSave: (state, action) => {
+      state.value = [...state.value, ...action.payload];
     },
-    reducers: {
-      messageSave: (state, action) => {
-        state.value =[...state.value ,...action.payload]
-    
-      },
     //   incrementByAmount: (state, action) => {
     //     state.value += action.payload;
     //   },
     clearMessages: (state) => {
-      state.value = []
+      state.value = [];
     },
-
+    adminMsgsDispatcher: (state, action) => {
+      state.adminMsgs = [...state.adminMsgs, ...action.payload];
     },
-    
-  });
-  
-  export const { messageSave,clearMessages } = slice.actions;
-  
-  export const storedMessages = state => state.messagesSaved.value;
-  
+  },
+});
 
-  export default slice.reducer;
+export const { messageSave, clearMessages, adminMsgsDispatcher } =
+  slice.actions;
+
+export const storedMessages = (state) => state.messagesSaved.value;
+export const adminMsgSelector = (state) => state.messagesSaved.adminMsgs;
+
+export default slice.reducer;
