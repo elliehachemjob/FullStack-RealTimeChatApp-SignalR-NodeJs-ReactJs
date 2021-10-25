1e2fa01a909bf3da4a61cf58c5fb90a7849a1c3b
Your Name <you@example.com>
2021-10-25 19:08:16 +0300

    minmal changes

--- story-book/src/stories/Client.stories.tsx
+++ story-book/src/stories/Client.stories.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { ClientWidget } from "../components/ClientWidget";
-import PropsTypes from "prop-types";
-// const stories = storiesOf("ClientWidget", module);
 
-// stories.add("ClientWidget", () => {
-//   return <ClientWidget />;
-// });
 export default {
   title: "ClientWidget",
   component: ClientWidget,
@@ -27,20 +22,6 @@ const ClientWidgetTemplate = (args: any) => <ClientWidget {...args} />;
 
 export const FullOptions = ClientWidgetTemplate.bind({});
 
-// ClientWidget.propTypes = {
-//   /**
-//    * Is this the principal call to action on the page?
-//    */
-//   isWidget: PropsTypes.bool,
-// };
-
-// ClientWidget.defaultProps = {
-//   isWidget: false,
-// };
-
 FullOptions.args = {
-  /**
-   * Is this the principal call to action on the page?
-   */
   isWidget: false,
 };
