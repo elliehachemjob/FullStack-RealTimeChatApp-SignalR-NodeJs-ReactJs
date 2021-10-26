b09a5ba04389898c92df75d09a50df1ccb029e38
Your Name <you@example.com>
2021-10-26 09:59:56 +0300

    minmal changes

--- story-book/src/stories/Admin.stories.tsx
+++ story-book/src/stories/Admin.stories.tsx
@@ -2,8 +2,32 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { AdminPanel } from "../components/AdminPanel";
 
-const stories = storiesOf("AdminPanel", module);
+// const stories = storiesOf("AdminPanel", module);
 
-stories.add("AdminPanel", () => {
-  return <AdminPanel />;
-});
+// stories.add("AdminPanel", () => {
+//   return <AdminPanel />;
+// });
+
+export default {
+  title: "AdminPanel",
+  component: AdminPanel,
+  argTypes: {
+    isWidget: {
+      description: "Changes Widget Animation",
+      table: {
+        type: {
+          summary: "Click For Summary",
+          detail: "Changes Widget Animation When Someone Togggles it",
+        },
+      },
+    },
+  },
+};
+
+const AdminWidgetTemplate = (args: any) => <AdminPanel {...args} />;
+
+export const FullOptions = AdminWidgetTemplate.bind({});
+
+FullOptions.args = {
+  username: "",
+};
