64947e94ffe65bde91efb666b088d0f3da93edd1
Your Name <you@example.com>
2021-10-27 17:07:30 +0300

    defining signup

--- React-Customer-Support/react-customer-support/src/components/Forms/Forms.tsx
+++ React-Customer-Support/react-customer-support/src/components/Forms/Forms.tsx
@@ -183,81 +183,100 @@ export const LoginForm: React.FC<Props> = (props: any): any => {
 };
 
 export const SignUpForm: React.FC<Props> = (props: any): any => {
-  const signUpHandler = () => {
-    axios
-      .post(`http://localhost:1589/api/users`, {
-        Email: user2,
-        Auth: password2,
-        IsAdmin: 0,
-      })
-      .then((res) => {
-        console.log(` data is ${JSON.stringify(res)}`);
-        // @ts-ignore
-        if (res.data === 'Added Successfully') {
-          alert('register success');
-          props.joinRoom(user2);
-          // @ts-ignore
-        } else if (res.data === 'Email Already Exist') {
-          alert('email already exist');
-        }
-      })
-      .catch((e) => {
-        console.log(e);
-        if (e) {
-          alert('already exist');
-        }
-      });
-  };
-
-  const [user2, setUser2] = useState<string>();
-  const [password2, setPassword2] = useState<string>();
-  const [sendEmailVerification, setSendEmailVerification] = useState<string>();
-
   return (
     <Level>
-      <Level.Item textAlign="centered">
+      <Level.Item textAlign={props.textAlign ? props.textAlign : 'centered'}>
         <Container
-          style={{
-            maxWidth: 400,
-            height: '100vh',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-          }}
+          style={
+            props.containerStyle
+              ? props.containerStyle
+              : {
+                  maxWidth: 400,
+                  height: '100vh',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                }
+          }
         >
           <Card>
-            <Section backgroundColor="primary">
-              <Title style={{ color: 'white' }}>Plugit Support Sign Up</Title>
+            <Section
+              backgroundColor={
+                props.titleBackground ? props.titleBackground : 'primary'
+              }
+            >
+              <Title
+                style={props.titleColor ? props.titleColor : { color: 'white' }}
+              >
+                {props.title ? props.title : 'Plugit Support Login'}
+              </Title>
             </Section>
             <Card.Content>
               <Field>
-                <Control iconLeft iconRight>
+                <Control
+                  iconLeft={props.fieldOneIconLeft ? props.emailIconLeft : true}
+                  iconRight={
+                    props.fieldOneIconRight ? props.fieldOneIconRight : true
+                  }
+                >
                   <Input
-                    // enabled={loading}
-                    type="email"
-                    placeholder="Email"
-                    onChange={(e: any) => {
-                      setUser2(e.target.value);
-                    }}
+                    type={props.fieldOneType ? props.fieldOneType : 'email'}
+                    placeholder={
+                      props.fieldOnePlaceholder
+                        ? props.fieldOnePlaceholder
+                        : 'email'
+                    }
+                    value={props.user}
+                    onChange={(e: any) => props.setUser(e.target.value)}
                   />
-                  <Icon size="small" align="left">
-                    <FontAwesomeIcon icon={faEnvelope} />
+
+                  <Icon
+                    size={props.faEnvelopeSize ? props.faEnvelopeSize : 'small'}
+                    align={
+                      props.faEnvelopeAlign ? props.faEnvelopeAlign : 'left'
+                    }
+                  >
+                    <FontAwesomeIcon
+                      icon={
+                        props.fieldOneIcon ? props.fieldOneIcon : faEnvelope
+                      }
+                    />
                   </Icon>
-                  <Icon size="small" align="right">
-                    <FontAwesomeIcon icon={faCheck} />
+                  <Icon
+                    size={props.checkIconSize ? props.checkIconSize : 'small'}
+                    align={
+                      props.checkIconAlign ? props.checkIconAlign : 'right'
+                    }
+                  >
+                    <FontAwesomeIcon
+                      icon={props.checkIcon ? props.checkIcon : faCheck}
+                    />
                   </Icon>
                 </Control>
               </Field>
               <Field>
-                <Control iconLeft>
+                <Control
+                  iconLeft={
+                    props.fieldTwoIconLeft ? props.fieldTwoIconLeft : true
+                  }
+                >
                   <Input
-                    type="password"
-                    placeholder="Password"
-                    onChange={(e: any) => setPassword2(e.target.value)}
-                    value={password2}
+                    type={props.fieldTwoType ? props.fieldTwoType : 'password'}
+                    placeholder={
+                      props.fieldTwoPlaceholder
+                        ? props.fieldTwoPlaceholder
+                        : 'password'
+                    }
+                    onChange={(e: any) => props.setPassword(e.target.value)}
+                    value={props.password}
                   />
-                  <Icon size="small" align="left">
-                    <FontAwesomeIcon icon={faLock} />
+                  <Icon
+                    size={props.faLockSize ? props.faLockSize : 'small'}
+                    align={props.faLockAlign ? props.faLockAlign : 'left'}
+                  >
+                    <FontAwesomeIcon
+                      icon={props.fieldTwoIcon ? props.fieldTwoIcon : faLock}
+                    />
                   </Icon>
                 </Control>
               </Field>
@@ -267,9 +286,9 @@ export const SignUpForm: React.FC<Props> = (props: any): any => {
                     type="email"
                     placeholder="Send Email Verification"
                     onChange={(e: any) =>
-                      setSendEmailVerification(e.target.value)
+                      props.setSendEmailVerification(e.target.value)
                     }
-                    value={sendEmailVerification}
+                    value={props.sendEmailVerification}
                   />
                   <Icon size="small" align="left">
                     <FontAwesomeIcon icon={faArrowRight} />
@@ -280,19 +299,22 @@ export const SignUpForm: React.FC<Props> = (props: any): any => {
                 <Control>
                   <Label>
                     <Checkbox />
-                    Remember me
+                    {props.rememberMe ? props.rememberMe : 'Remember me'}
                   </Label>
+                  <Label></Label>
                 </Control>
               </Field>
               <Field>
                 <Control>
                   <Button
                     onClick={() => {
-                      signUpHandler();
+                      props.loginHandler();
                     }}
                     color="primary"
                   >
-                    SignUp
+                    {props.loginHandlertitle
+                      ? props.loginHandlertitle
+                      : 'Login'}
                   </Button>
                 </Control>
               </Field>
