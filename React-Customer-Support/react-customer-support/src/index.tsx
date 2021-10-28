bfe2017e758b451fd80a83e16e61e6bbb778007e
Your Name <you@example.com>
2021-10-28 13:51:41 +0300

    changing

--- React-Customer-Support/react-customer-support/src/index.tsx
+++ React-Customer-Support/react-customer-support/src/index.tsx
@@ -7,3 +7,9 @@ export * from './components/AdminPanel';
 export * from './components/AdminPanelFilled';
 
 export * from './components/ClientWidget';
+
+export * from './components/VoiceCall';
+
+export * from './components/VideoCall';
+
+export * from './components/SidebarExpansion';
