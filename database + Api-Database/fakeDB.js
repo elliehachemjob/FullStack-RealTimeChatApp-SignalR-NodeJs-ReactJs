2ad21230de981493350a07697a70889ad252ad5c
KaryBozkurt <kary.r.bozkurt@gmail.com>
2024-10-06 11:32:23 +0200

    Update fakeDB.js

--- database + Api-Database/fakeDB.js
+++ database + Api-Database/fakeDB.js
@@ -2,6 +2,8 @@ exports.fakeDB = [
   {
     id: 0,
     email: 'elliehachem@gmail.com',
+    //email:'kary.r.bozkurt@gmail.com'
+    //password:'mewDataBase321'
     password: 'HACEHem123*', // Password: test
   },
 ];
